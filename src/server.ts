f8db35f8c7e7951165fbf67db3ae3cc67d7b35b9
iamhacker1997 <quyen.nv153086@sis.hust.edu.vn>
2021-04-12 18:33:21 +0700

    https

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,25 @@ dotenv.config();
 
 import app from './app';
 import { logger } from './logger';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
 const PORT = process.env.PORT || 1234;
 
-app.listen(PORT, () => {
-  logger.info(`Running on port ${PORT}`);
+const credentials = {
+  key: fs.readFileSync(path.join('sslcert', 'techqila.key'), 'utf8'),
+  cert: fs.readFileSync(path.join('sslcert', 'techqila.crt'), 'utf8'),
+}
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(PORT, () => {
+  logger.info(`Running http on port ${PORT}`);
+});
+
+httpsServer.listen(9443, () => {
+  logger.info(`Running http on port 9443`);
 });
